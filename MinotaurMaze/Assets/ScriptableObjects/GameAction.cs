0ac113105fb7466141755d5d6c11d84310bb2acc
Peter Fluit <fluitpd@gmail.com>
2023-02-13 20:01:52 -0700

    Added Health prefabs and HealthAction

---
+++ MinotaurMaze/Assets/ScriptableObjects/GameAction.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Actions/Game Action")]
+
+public class GameAction : ScriptableObject
+{
+   public UnityAction raiseNoArgs;
+
+   public void RaiseAction()
+   {
+      raiseNoArgs?.Invoke();
+   }
+
+}
--- MinotaurMaze/Assets/ScriptableObjects/ImageBehaviour.cs
+++ MinotaurMaze/Assets/ScriptableObjects/ImageBehaviour.cs
@@ -5,12 +5,19 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Image))]
 public class ImageBehaviour : MonoBehaviour
 {
+    public GameAction gameActionObj;
     public UnityEvent startEvent;
     private Image img;
 
     void Start()
     {
+        gameActionObj.raiseNoArgs += RunStartEvent;
         img = GetComponent<Image>();
+        startEvent.Invoke();
+    }
+
+    private void RunStartEvent()
+    {
         startEvent.Invoke();
     }
 
